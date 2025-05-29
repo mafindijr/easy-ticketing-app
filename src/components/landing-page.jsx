828af4a2e04c3074a742a00684c40929b0af1992
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-29 14:47:15 -0700

    style: adde some style to the input

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -27,9 +27,9 @@ export default function LandingPage() {
 
          <div className="right-0 left-0 inline-flex  absolute top-12 bg-gray-100 px-10 z-10">
       
-          <input type="search" className="flex-1 bg-gray-200 border-none rounded-sm mb-2 mt-2 py-2 px-4 font-sm inline-flex "  name="search" placeholder="Search for concerts, tech meetup or more" />
+          <input type="search" className="flex-1  border-1 border-gray-200 rounded-sm mb-2 mt-2 py-2 px-4 font-sm shadow-inset-gray-300 inline-flex "  name="search" placeholder="Search for concerts, tech meetup or more" />
           
-          <button className="bg-gray-200 border-1 border-gray-100 cursor-pointer w-20 px-4 py-2 mt-2 mb-2">Lagos</button>
+          <button className="bg-gray-200 border-1  border-gray-200 cursor-pointer w-20 px-4 py-2 mt-2 mb-2">Lagos</button>
           <button className="bg-gray-200 border-1 border-gray-100 cursor-pointer w-20 px-4 py-2 mt-2 mb-2">Paid</button>
           
           </div>

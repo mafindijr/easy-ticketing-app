b7c628c6ac9f38a9d01e4642abfb2c60a5752b1c
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-31 03:08:49 -0700

    refactor: change all the values from the h1 and p tag to normal sstring for testig purpose

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,30 +1,30 @@
 
 
 
-export default function Card () {
+export default function Card (props) {
 
     return (
-        <div className="max-w-xs  border border-gray-700 rounded-xl shadow-md overflow-hidden bg-gray-800">
+        <div className="max-w-xs  border border-gray-100 rounded-xl overflow-hidden bg-gray-50">
           <img
-            src={events.thumb}
-            alt="Grilled Chicken Salad"
+            src=""
+            alt=""
             className="w-full h-52 object-cover"
           />
-          <div className="p-4">
-            <h2 className="text-xl font-semibold mb-2 text-white">{events.name}</h2>
+          <div className="mb-4 p-4">
+            <h2 className="text-xl font-semibold mb-2 ">name</h2>
             <p>
-                {events.date}
+                date
             </p>
-            <p className="text-gray-300">
-              {events.location}
+            <p className="">
+              location
             </p>
             <p>
-                {events.price}
+                price
             </p>
           </div>
 
           <div>
-            <button>Book Now</button>
+            <button className="bg-homeexplore text-gray-50 ml-4 mb-4 px-4 py-1 rounded-sm">Book Now</button>
           </div>
         </div>
     );

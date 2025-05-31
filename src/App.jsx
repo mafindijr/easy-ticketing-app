45ce0d1b03c2f3cd0963e8fa1732582f0852c286
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-31 03:07:43 -0700

    render: render the card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import LandingPage from "./components/landing-page";
+import Card from "./components/Cards";
 
 function App() {
 
   return (
     <>
       <LandingPage />
+
+        <h1>Top Events</h1>
+      <div className='grid grid-cols-4 gap-4'>
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   )
 }

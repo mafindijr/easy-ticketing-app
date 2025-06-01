b11d52f26430d791cd608216266cbc8f258d6333
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-01 04:16:03 -0700

    Rendeer: imported and render the categories flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import LandingPage from "./components/landing-page";
 import CardsList from "./components/cardlist";
+import Categories from './components/categories';
 
 function App() {
 
   return (
     <>
       <LandingPage />
+      <h1>Explore top categories</h1>
+      <Categories />
 
         <h1>Top Events</h1>
       <div className='grid grid-cols-4 gap-4'>

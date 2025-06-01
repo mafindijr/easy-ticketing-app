0ea1219298dee0bcd43bc87e49de6e79e8880056
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-01 03:34:43 -0700

    Component: created a category component

---
+++ src/components/categories.jsx
@@ -0,0 +1,46 @@
+
+import React from 'react'
+
+export default function Categories() {
+
+        const eventsCategory = [
+            {
+                svgIcon: ``,
+                title: 'Music & Concerts'
+            },
+            {
+                svgIcon: ``,
+                title: 'Cultural & Traditional Festival'
+            },
+            {
+                svgIcon: ``,
+                title: 'Corporate & Professional Events'
+            },
+            {
+                svgIcon: ``,
+                title: 'Trade Shows & Expos'
+            },
+            {
+                svgIcon: ``,
+                title: 'Religios & and Community Gatherings'
+            },
+            {
+                svgIcon: ``,
+                title: 'Wellness & Lifestyle Events'
+            },
+            {
+                svgIcon: ``,
+                title: 'Sports & Outdoor Events'
+            },
+            {
+                svgIcon: ``,
+                title: 'Art, Fashion & Creative Events'
+            },
+        ];
+
+  return (
+    <div>
+
+    </div>
+  )
+}

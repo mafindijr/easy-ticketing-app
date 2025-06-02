0ee9494f0f1ccb77fb086e18c9bdfe1a3bdda628
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-02 14:12:46 -0700

    Temporary: i added temporary text  for thhe development

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      
+      <div>
+        <h1>Host Your Next Event!</h1>
+        <button>Create Event Now</button>
+
+        <div>
+          <img src="" alt="" />
+        </div>
+
+      </div>
+
+      <div>
+        <div>
+          About easy Ticketing
+          contact us
+          privacy policy
+          terms of use
+        </div>
+        <div>
+          @ 2025 EasyTicket. All right reserved
+        </div>
+        <div>
+          follow us 
+          linkin
+          facebook
+          x
+        </div>
+      </div>
+
     </div>
   )
 }

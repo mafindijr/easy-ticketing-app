7f00f217dc00d0ae85642d94073a42f334707ee4
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-04 10:03:55 -0700

    fix: wrap main content in Layout component for better structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,27 @@ import LandingPage from "./components/landing-page";
 import Card from "./components/Cards";
 import Categories from './components/categories';
 import Footer from './components/Footer';
+import Layout from './layout/layout'
 
 function App() {
 
   return (
     <>
       <LandingPage />
-      <h1 className='ml-30'>Explore top categories</h1>
-      <Categories />
 
-        <h1 className='ml-30'>Top Events</h1>
-      <div className='grid grid-cols-4 gap-4 w-full px-30'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <Layout>
+        <h1 className='ml-30'>Explore top categories</h1>
+        <Categories />
+
+          <h1 className='ml-30'>Top Events</h1>
+        <div className='grid grid-cols-4 gap-4 w-full px-30'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+
+      </Layout>
 
       <Footer />
     </>

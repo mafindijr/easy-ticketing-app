a11fbaa34abfb47fd4e6f311a8a2f2e1a6b5c353
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-05 10:22:16 -0700

    fix: update svgIcons for event categories and improve layout in Categories component

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import { MicVocal } from 'lucide-react'
+import { MicVocal, Lectern } from 'lucide-react'
+import { FaHandshake } from "react-icons/fa6";
+import { PiMaskSad } from "react-icons/pi";
+
 
 export default function Categories() {
 
         const eventsCategory = [
             {
                 id: 1,
-                svgIcon: <MicVocal />,
+                svgIcon: <MicVocal size={32} />,
                 title: 'Music & Concerts'
             },
             {
                 id: 2,
-                svgIcon: ``,
+                svgIcon: <PiMaskSad size={32} />,
                 title: 'Cultural & Traditional Festival'
             },
             {
                 id: 3,
-                svgIcon: ``,
+                svgIcon: <Lectern size={32} />,
                 title: 'Corporate & Professional Events'
             },
             {
                 id: 4,
-                svgIcon: ``,
+                svgIcon: <FaHandshake size={32} />,
                 title: 'Trade Shows & Expos'
             },
             {
@@ -43,20 +46,24 @@ export default function Categories() {
                 id:  8,
                 svgIcon: ``,
                 title: 'Art, Fashion & Creative Events'
-            },
+            },us
         ];
 
   return (
     <div className='flex flex-row gap-4 items-center justify-around pl-30 pr-30 my-4'>
         
         {eventsCategory.map(category => (
-            <div key={category.id} className='w-[137px] h-[118px] border border-gray-200 p-[8px] overflow-hidden text-[16px] leading-[22px] rounded-md'>
+            <div
+                key={category.id}
+                className='w-[170px] h-[130px] border border-gray-200 p-3 overflow-hidden text-[16px] leading-[22px] rounded-md flex flex-col items-center'
+            >
                <span className='flex justify-center items-center mb-4 text-homeexplohover'>
                  {category.svgIcon}
                </span>
-               <p>{category.title}</p>
+               <p className='text-center break-words whitespace-normal'>
+                 {category.title}
+               </p>
             </div>
-            
         ))}
 
     </div>

51b8c14c8fc87b3226f5dd89a27b974b2b165d31
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-05 09:07:29 -0700

    fix: refactor Footer component to use navLinks array for navigation and improve layout structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,17 @@ import { BsTwitterX  } from "react-icons/bs";
 
 
 export default function Footer() {
+
+  const navLinks = [
+  { name: "About EasyTicketing", href: "#" },
+  { name: "Contact Us", href: "#" },
+  { name: "Privacy Policy", href: "#" },
+  { name: "Terms of Use", href: "#" },
+];
+
+
+
+
   return (
     <>
     <div className='bg-[#eeeeee] flex justify-center items-center h-[400px] py-8'>
@@ -30,10 +41,15 @@ export default function Footer() {
       <div className='bg-[#0D47A1] flex justify-between pr-30 pl-30 py-4'>
 
         <div className='flex flex-col items-start text-gray-50'>
-          <a href="#">About EasyTicketing</a>
+
+         {navLinks.map((link) => (
+            <a key={link.name} href={link.href}>{link.name}</a>
+          ))}
+
+          {/* <a href="#">About EasyTicketing</a>
           <a href="#">Contact Us</a>
           <a href="#">Privacy Policy</a>
-          <a href="#">Terms of Use</a>
+          <a href="#">Terms of Use</a> */}
         </div>
 
         <div className='text-gray-50'>
@@ -41,10 +57,14 @@ export default function Footer() {
         </div>
 
         <div className='flex items-center gap-4 text-gray-50 relative'>
-          <span className='absolute top-0 left-0'>Follow Us</span>
-          <div className='border-2 border-solid-gray-50 p-1 rounded-full'><a href="#"><Linkedin size={24} /></a></div>
-          <div className='border-2 border-solid-gray-50 p-1 rounded-full'><a href="#"><Facebook size={24} /></a></div>
-          <div className='border-2 border-solid-gray-50 p-2 text-xl rounded-full'><a href=""><BsTwitterX /></a></div>
+          <div className='flex flex-col items-start'>
+            <span className='mb-3'>Follow Us</span>
+            <div className='flex items-center gap-4'>
+              <div className='border-2 border-solid-gray-50 p-1 rounded-full'><a href="#"><Linkedin size={24} /></a></div>
+              <div className='border-2 border-solid-gray-50 p-1 rounded-full'><a href="#"><Facebook size={24} /></a></div>
+              <div className='border-2 border-solid-gray-50 p-2 text-xl rounded-full'><a href=""><BsTwitterX /></a></div>
+            </div>
+          </div>
         </div>
       </div>
 

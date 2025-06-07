12f95dc08dd31e6ccc553168e010ae861efbed8f
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-07 15:41:00 -0700

    fix: check if events is equal to zero or falsy not to render a event

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Card from './Cards';
 
-export default function EventSection() {
+export default function EventSection({ title, events, layout = 'grid' }) {
+
+  if(!events || events.lenght === 0) return null;
+
+  return ();
 
   return (
     <div>TopEvents</div>

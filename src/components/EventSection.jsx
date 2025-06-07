f3033805161a810bb04126fc0e70e8dc6b8fcb3d
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-07 15:37:50 -0700

    fix: change he event function name to eventsection for better referencing

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Card from './Cards';
 
-export default function TopEvents() {
+export default function EventSection() {
+
   return (
     <div>TopEvents</div>
   )

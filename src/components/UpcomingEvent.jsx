87bc0b5ab62bac395d8f1b06a33db49543b5a1cb
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-08 03:25:36 -0700

    style: enhance layout and grid configuration in UpcomingEvents component

--- src/components/UpcomingEvent.jsx
+++ src/components/UpcomingEvent.jsx
@@ -5,9 +5,9 @@ function UpcomingEvents() {
   const upcomingEvents = events.filter((event) => event.type === "upcoming");
 
   return (
-    <div className="my-8">
+    <div className="w-full pr-30 pl-30 my-8">
       <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-2">
         {upcomingEvents.map((event, idx) => (
           <Card key={idx} {...event} showBookButton={false} />
         ))}

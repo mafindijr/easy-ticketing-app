567735d6c4a371131c693a1a1b9891565430c2b9
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-08 03:25:23 -0700

    feat: restore Slider and styles in TopEvents component for carousel functionality

--- src/components/TopEvent.jsx
+++ src/components/TopEvent.jsx
@@ -1,28 +1,28 @@
-// import Slider from "react-slick";
+import Slider from "react-slick";
 import Card from "./Cards";
 import events from "../components/javascript/events";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function TopEvents() {
   const topEvents = events.filter((event) => event.type === "top");
 
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     arrows: true,
-//   };
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+  };
 
   return (
-    <div className="my-8">
+    <div className="w-full pr-30 pl-30 my-8">
       <h2 className="text-2xl font-bold mb-4">Top Events</h2>
-      {/* <Slider {...settings}> */}
+      <Slider {...settings}>
         {topEvents.map((event, idx) => (
           <Card key={idx} {...event} showBookButton={true} />
         ))}
-      {/* </Slider> */}
+      </Slider>
     </div>
   );
 }

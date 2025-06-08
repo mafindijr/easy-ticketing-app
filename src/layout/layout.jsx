e9397bb043cc3c73a9b0cf863c136a8aabb036ca
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-08 02:16:55 -0700

    feat: enhance layout component by adding Header and Footer

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import LandingPage from '../components/landing-page'
+import Footer from '../components/Footer'
 
 export default function layout({ children }) {
   return (
     <div>
+
+       {/* Header Component */}
+      <LandingPage />
+
+      <main className='flex-1'>
         {children}
+      </main>
+
+      <Footer />
+        
     </div>
   )
 }

c110da8a7c3e22575d753b68827f34df70a0da6f
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-08 03:05:07 -0700

    refactor: clean up Home component by commenting out unused Card import and related code

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,8 @@
-import Card from "../components/Cards";
+// import Card from "../components/Cards";
 import Categories from '../components/categories';
-import Layout from '../layout/layout'
+import Layout from '../layout/layout';
+import TopEvents from "../components/TopEvent";
+import UpcomingEvents from "../components/UpcomingEvent";
 
 
 export default function Home() {
@@ -12,15 +14,20 @@ export default function Home() {
       <Layout>
 
         <h1 className='text-2xl font-bold my-4 ml-30'>Explore top categories</h1>
+
         <Categories />
 
-          <h1 className='ml-30'>Top Events</h1>
+        
+          <TopEvents />
+          <UpcomingEvents />
+
+          {/* <h1 className='ml-30'>Top Events</h1>
         <div className='grid grid-cols-4 gap-4 w-full px-30'>
+          <Card />c
           <Card />
           <Card />
           <Card />
-          <Card />
-        </div>
+        </div> */}
 
         <div className='flex justify-center align-middle p-4'>
           <button className='border border-gray-400 w-[380px] h-[60px] text-[16px] leading-[24px] rounded-sm cursor-pointer'>See More</button>
@@ -29,3 +36,10 @@ export default function Home() {
       </Layout>
   )
 }
+
+
+
+
+
+
+

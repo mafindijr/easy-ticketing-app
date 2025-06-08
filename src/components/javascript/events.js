4d24387ed962a2f9107383ec7b5f866ce3006d3b
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-08 03:09:22 -0700

    feat: add events data for upcoming and top events

---
+++ src/components/javascript/events.js
@@ -0,0 +1,44 @@
+const events = [
+  {
+    name: "Music Fest",
+    date: "2025-06-15",
+    location: "Lagos",
+    price: "₦5000",
+    image: "/card-images/easy-image.jpg",
+    type: "top",
+  },
+  {
+    name: "Art Expo",
+    date: "2025-06-20",
+    location: "Abuja",
+    price: "₦3000",
+    image: "/card-images/easy-image.jpg",
+    type: "top",
+  },
+  {
+    name: "Tech Summit",
+    date: "2025-07-01",
+    location: "Port Harcourt",
+    price: "₦7000",
+    image: "/card-images/easy-image.jpg",
+    type: "top",
+  },
+  {
+    name: "Comedy Night",
+    date: "2025-07-10",
+    location: "Ibadan",
+    price: "₦2500",
+    image: "/card-images/easy-image.jpg",
+    type: "upcoming",
+  },
+  {
+    name: "Food Carnival",
+    date: "2025-07-15",
+    location: "Enugu",
+    price: "₦4000",
+    image: "card-images/easy-image.jpg",
+    type: "upcoming",
+  },
+];
+
+export default events;

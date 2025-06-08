53d2f3ba2106155422d416037d38444c3a35ce2a
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-08 03:13:12 -0700

    feat: add UpcomingEvents component to display a list of upcoming events

---
+++ src/components/UpcomingEvent.jsx
@@ -0,0 +1,19 @@
+import Card from './Cards';
+import events from "../components/javascript/events";
+
+function UpcomingEvents() {
+  const upcomingEvents = events.filter((event) => event.type === "upcoming");
+
+  return (
+    <div className="my-8">
+      <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {upcomingEvents.map((event, idx) => (
+          <Card key={idx} {...event} showBookButton={false} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default UpcomingEvents;

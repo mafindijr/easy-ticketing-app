c09e03494a36bb83f2a26a6d41c23428b393cfe4
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-11 09:36:26 -0700

    refactor: enhance layout of NavigationIcon component with flexbox and consistent button styling

--- src/components/Navigation-icon.jsx
+++ src/components/Navigation-icon.jsx
@@ -3,14 +3,14 @@ import { ChevronRight, ChevronLeft } from 'lucide-react'
 export default function NavigationIcon() {
 
   return (
-    <div>
+    <div className='flex gap-4'>
 
         <button className='bg-gray-100 rounded-full p-2'>
             <ChevronLeft size={30}/>
         </button>
 
-        <button>
-          <ChevronRight />
+        <button className='bg-gray-200 rounded-full p-2'>
+          <ChevronRight size={30}/>
         </button>
     </div>
   );

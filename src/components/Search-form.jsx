e0bc505b14baec129f5d9ec2aaf8c72c3a6bd563
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-11 07:34:23 -0700

    fix: remove unnecessary border from button in SearchForm component

--- src/components/Search-form.jsx
+++ src/components/Search-form.jsx
@@ -55,7 +55,7 @@ export default function SearchForm ({ search, setSearch, handleSearch }) {
                 </div>
                 )}
                 </div>
-                <button className="bg-homeexplore hover:bg-homeexplohover hover:bg-gray-100 border-1 border-gray-200  cursor-pointer rounded-r-full w-9 h-9 mb-2 mt-2 flex items-center justify-center">
+                <button className="bg-homeexplore hover:bg-homeexplohover  cursor-pointer rounded-r-full w-9 h-9 mb-2 mt-2 flex items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>

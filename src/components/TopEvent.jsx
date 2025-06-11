e1dcde0b816bc5724be39283c2e1bb0d40874060
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-11 09:36:33 -0700

    refactor: improve layout of TopEvents component for better structure and readability

--- src/components/TopEvent.jsx
+++ src/components/TopEvent.jsx
@@ -8,17 +8,20 @@ function TopEvents() {
 
   return (
     <div className="w-full pr-30 pl-30 my-8">
-      <div>
-          <div><h2 className="text-2xl font-bold mb-4">Top Events</h2></div>
+      <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-2xl font-bold mb-4">Top Events</h2>
+            </div>
           <div>
             <NavigationIcon />
           </div>
       </div>
       
-      
-      {/* {topEvents.map((event, idx) => (
-          <Card key={idx} {...event} showBookButton={true} />
-        ))} */}
+      <div className="grid grid-cols-4 gap-4 py-4">      
+         {topEvents.map((event, idx) => (
+            <Card key={idx} {...event} showBookButton={false} />
+          ))} 
+      </div>
     </div>
   );
 }

3da37e962d9a5e147feeda788ea1bd13f44f1177
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-11 07:34:44 -0700

    refactor: remove Slider component and simplify TopEvents layout

--- src/components/TopEvent.jsx
+++ src/components/TopEvent.jsx
@@ -1,28 +1,24 @@
-import Slider from "react-slick";
 import Card from "./Cards";
 import events from "../components/javascript/events";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { NavigationIcon } from "./Navigation-icon";
+
 
 function TopEvents() {
   const topEvents = events.filter((event) => event.type === "top");
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-  };
-
   return (
     <div className="w-full pr-30 pl-30 my-8">
-      <h2 className="text-2xl font-bold mb-4">Top Events</h2>
-      <Slider {...settings}>
-        {topEvents.map((event, idx) => (
+      <div>
+          <div><h2 className="text-2xl font-bold mb-4">Top Events</h2></div>
+          <div>
+            <NavigationIcon />
+          </div>
+      </div>
+      
+      
+      {/* {topEvents.map((event, idx) => (
           <Card key={idx} {...event} showBookButton={true} />
-        ))}
-      </Slider>
+        ))} */}
     </div>
   );
 }

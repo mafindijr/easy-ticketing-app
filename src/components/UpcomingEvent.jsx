b203bb7df61d7456f182ee1c7fcab1508f9b495e
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-11 06:48:57 -0700

    feat: simplify layout of Card and UpcomingEvents components by removing unnecessary classes

--- src/components/UpcomingEvent.jsx
+++ src/components/UpcomingEvent.jsx
@@ -5,7 +5,7 @@ function UpcomingEvents() {
   const upcomingEvents = events.filter((event) => event.type === "upcoming");
 
   return (
-    <div className="w-full pr-30 pl-30 my-8">
+    <div className="pr-30 pl-30 my-8">
       <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-2">
         {upcomingEvents.map((event, idx) => (

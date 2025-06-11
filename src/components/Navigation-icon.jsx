6a9afb49448613203df9ea82148fcae940044395
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-11 09:57:43 -0700

    refactor: update NavigationIcon component to accept onLeft and onRight props for better functionality

--- src/components/Navigation-icon.jsx
+++ src/components/Navigation-icon.jsx
@@ -1,17 +1,22 @@
 import { ChevronRight, ChevronLeft } from 'lucide-react'
 
-export default function NavigationIcon() {
-
+export default function NavigationIcon({ onLeft, onRight }) {
   return (
     <div className='flex gap-4'>
-
-        <button className='bg-gray-100 rounded-full p-2'>
-            <ChevronLeft size={30}/>
-        </button>
-
-        <button className='bg-gray-200 rounded-full p-2'>
-          <ChevronRight size={30}/>
-        </button>
+      <button
+        className='bg-gray-100 hover:bg-gray-200 rounded-full p-1'
+        onClick={onLeft}
+        type="button"
+      >
+        <ChevronLeft size={30}/>
+      </button>
+      <button
+        className='bg-gray-100 hover:bg-gray-200 rounded-full p-1'
+        onClick={onRight}
+        type="button"
+      >
+        <ChevronRight size={30}/>
+      </button>
     </div>
   );
 }

9c2385aae4043649811d65e8b6f4a28aa66db7ca
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-11 09:43:27 -0700

    refactor: reorganize UpcomingEvents component layout for improved readability

--- src/components/UpcomingEvent.jsx
+++ src/components/UpcomingEvent.jsx
@@ -6,12 +6,16 @@ function UpcomingEvents() {
 
   return (
     <div className="w-full pr-30 pl-30 my-8">
-      <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
-      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {upcomingEvents.map((event, idx) => (
-          <Card key={idx} {...event} showBookButton={true} />
-        ))}
+      <div>
+          <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
       </div>
+
+      <div className='grid grid-cols-4 gap-4 py-4'>
+          {upcomingEvents.map((event, idx) => (
+            <Card key={idx} {...event} showBookButton={true} />
+          ))} 
+      </div>
+      
     </div>
   );
 }

7ad0d4901727134633fddd1113ff7aa20fa20bc1
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-14 07:29:41 -0700

    refactor: add location icon and link to Upcoming Events section

--- src/components/UpcomingEvent.jsx
+++ src/components/UpcomingEvent.jsx
@@ -1,5 +1,6 @@
 import Card from './Cards';
 import events from "../components/javascript/events";
+import { IoLocationSharp } from "react-icons/io5";
 
 function UpcomingEvents() {
   const upcomingEvents = events.filter((event) => event.type === "upcoming");
@@ -8,6 +9,7 @@ function UpcomingEvents() {
     <div className="w-full pr-30 pl-30 my-8">
       <div>
           <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
+          <a href="#"> <IoLocationSharp size={18} /> Lagos </a>
       </div>
 
       <div className='grid grid-cols-4 gap-4 py-4'>

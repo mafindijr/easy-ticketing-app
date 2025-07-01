eb9830bedb2ead0c37f09727807f9dd548be4ccf
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-01 15:58:56 -0700

    feat: create profile component structure

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,32 @@
-
+import { useForm } from "react-hook-form";
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <header>
+        <div>
+        <a href="#">Home</a>
+        </div>
+        <div>
+          <a href="#">Profile</a>
+          <a href="#">Update Dashbord</a>
+          <a href="#">Log Out</a>
+        </div>
+      </header>
+
+      <div>
+        <nav>
+      
+        </nav>
+        <main>
+            <h1>Personal Information</h1>
+            <p>Update your</p>
+
+            <form>
+
+            </form>
+        </main>
+      </div>
+    </div>
   )
 }

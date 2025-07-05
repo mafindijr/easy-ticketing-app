b96de26e260cdac47b96cf6416fa3303f6410b43
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-04 23:12:55 -0700

    import profile component for preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Home from './views/Home'
+import Home from './views/Home';
+import Profile from './components/Profile';
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
     <>
 
     <Home />
-
+    <Profile />
     </>
   )
 }

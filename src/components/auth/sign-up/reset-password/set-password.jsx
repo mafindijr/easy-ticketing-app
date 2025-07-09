2f12d2dfae9d3cdb6b4087978b07949e93ebdae2
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-09 00:38:24 -0700

    set password component done

--- src/components/auth/sign-up/reset-password/set-password.jsx
+++ src/components/auth/sign-up/reset-password/set-password.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { Input } from "../../../input";
+import { Button } from "../../../button";
+import { useForm } from "react-hook-form";
 
 export default function SetPassword() {
+
+        const { register, handeSubmit, stateForm: { errors }} = useForm();
+
   return (
     <div>
         <h2 className='font-montserrat font-[700] font-bold text-[32px] leading-[44px] text-center'>Set New Password</h2>
@@ -8,7 +13,7 @@ export default function SetPassword() {
 
         
                         <Input
-                            label="Password"
+                            label="New Password"
                             name="password"
                             type="password"
                             placeholder="New Password"
@@ -16,20 +21,21 @@ export default function SetPassword() {
                             error={errors.password}
                             required="Password is required"
                         />
+                        <span className='font-montserrat cursor-pointer font-[400] text-[18px] leading-[28px]'>Password must be atleast 8 characters</span>
         
                         <Input
                             label="Confirm new Password"
                             name="confirmPassword"
                             type="password"
-                            placeholder="Confirm your password"
+                            placeholder="Confirm new password"
                             register={register}
                             error={errors.confirmPassword}
                             required="Please confirm your password"
                         />
 
                         
-                        <Button type='submit' className='w-full mt-6 font-poppins h-[50px] font-bold bg-blue-600 text-white py-2 rounded-[8px]' onClick={() => setOpenResetPassword(true)}>
-                          send Reset Link
+                        <Button type='submit' className='w-full mt-6 font-poppins h-[50px] font-bold bg-blue-600 text-white py-2 rounded-[8px]'>
+                          Update Password
                         </Button>
                         <span className='font-montserrat cursor-pointer font-[400] text-[18px] leading-[28px]'>Back to log in</span>
     </div>

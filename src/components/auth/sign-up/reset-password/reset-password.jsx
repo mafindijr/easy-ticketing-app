1b41b516f896b2483a8b85990e21740804808986
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-09 05:52:47 -0700

    fix: add some styling to the for consistency

--- src/components/auth/sign-up/reset-password/reset-password.jsx
+++ src/components/auth/sign-up/reset-password/reset-password.jsx
@@ -14,7 +14,7 @@ export default function ResetPassword() {
         <>
           {!openResetPassword && (
             <div>
-                <h2 className='font-montserrat font-[700] font-bold text-[32px] leading-[44px] text-center'>Reset Your Password</h2>
+                <h2 className='font-montserrat font-[700] font-bold text-xs leading-[44px] text-center'>Reset Your Password</h2>
                 <p className='font-montserrat font-[400] text-[18px] leading-[28px] text-center'>Enter your email to receive a reset link</p>
                 <Input 
                       label="Email"
@@ -29,7 +29,7 @@ export default function ResetPassword() {
                 <Button type='submit' className='w-full mt-6 font-poppins h-[50px] font-bold bg-blue-600 text-white py-2 rounded-[8px]' onClick={() => setOpenResetPassword(true)}>
                   send Reset Link
                 </Button>
-                <span className='font-montserrat cursor-pointer font-[400] text-[18px] leading-[28px]'>Back to log in</span>
+                <span className='font-montserrat cursor-pointer font-[400] text-xs'>Back to log in</span>
 
             </div>
           )}

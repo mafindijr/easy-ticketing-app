3da1f45d202d9c700499f30e009f1f8c409cc12b
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-09 00:04:11 -0700

    feat: create the head and paragraph text

--- src/components/auth/sign-up/reset-password/set-password.jsx
+++ src/components/auth/sign-up/reset-password/set-password.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function SetPassword() {
   return (
-    <div>set-passwoP</div>
+    <div>
+        <h2 className='font-montserrat font-[700] font-bold text-[32px] leading-[44px] text-center'>Set New Password</h2>
+        <p className='font-montserrat font-[400] text-[18px] leading-[28px] text-center'>Create a new password for your account</p>
+
+        
+    </div>
   )
 }

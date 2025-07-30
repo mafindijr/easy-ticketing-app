48b93c8917406ad5909fad2adc19ef7afa070b27
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-30 12:36:12 -0700

    refactor: update heading and button styles in reset password component

--- src/components/auth/sign-up/reset-password/reset-password.jsx
+++ src/components/auth/sign-up/reset-password/reset-password.jsx
@@ -14,7 +14,7 @@ export default function ResetPassword() {
         <>
           {!openResetPassword && (
             <div>
-                <h2 className='font-montserrat font-[700] font-bold text-xs leading-[44px] text-center'>Reset Your Password</h2>
+                <h4 className='font-montserrat font-[700] font-bold text-[20px] leading-[30px] text-center mb-2'>Reset Your Password</h4>
                 <p className='font-montserrat font-[400] text-[18px] leading-[28px] text-center'>Enter your email to receive a reset link</p>
                 <Input 
                       label="Email"
@@ -26,10 +26,19 @@ export default function ResetPassword() {
                       required
                 />
 
-                <Button type='submit' className='w-full mt-6 font-poppins h-[50px] font-bold bg-blue-600 text-white py-2 rounded-[8px]' onClick={() => setOpenResetPassword(true)}>
+                <Button type='submit' 
+                className='w-full mt-6 font-poppins h-[50px] font-bold bg-blue-600 text-white py-2 rounded-[8px]' 
+                onClick={() => setOpenResetPassword(true)}
+                style={{boxShadow: "0px 6px 6px rgba(2, 2, 2, 0.15)"}}
+                >
                   send Reset Link
                 </Button>
-                <span className='font-montserrat cursor-pointer font-[400] text-xs'>Back to log in</span>
+                 <Button
+                      className="py-2 px-8 mt-6 w-full border border-[#CCCCCC] rounded-[8px] font-poppins font-bold text-[16px] text-center gap-30"
+                      style={{boxShadow: "0px 0px 2px 3px rgba(2, 2, 2, 0.15)"}}
+                  >
+                      Back to Log In
+                  </Button>
 
             </div>
           )}

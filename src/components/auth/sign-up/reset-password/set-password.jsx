3932ac8a667c5c09740941e88b25b245856c8532
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-31 02:44:00 -0700

    fix: added a buttpn to login page

--- src/components/auth/sign-up/reset-password/set-password.jsx
+++ src/components/auth/sign-up/reset-password/set-password.jsx
@@ -37,7 +37,13 @@ export default function SetPassword() {
                         <Button type='submit' className='w-full mt-6 font-poppins h-[50px] font-bold bg-blue-600 text-white py-2 rounded-[8px]'>
                           Update Password
                         </Button>
-                        <span className='font-montserrat cursor-pointer font-[400] text-xs'>Back to log in</span>
+                        <Button
+                            className="py-2 px-8 mt-6 w-full border border-[#CCCCCC] rounded-[8px] font-montserrat font-bold text-[16px] text-[700] text-center gap-30"
+                            style={{boxShadow: "0px 0px 2px 3px rgba(2, 2, 2, 0.15)"}}
+                        >
+                            Back to Log In
+                        </Button>
+                        
     </div>
   )
 }

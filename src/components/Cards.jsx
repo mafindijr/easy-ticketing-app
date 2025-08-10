1ec221c756dda292eb1b745147c26793b858cd24
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-08-10 01:37:58 -0700

    fix: the back to ticket arrow selection to the ticket selection dialog

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -218,8 +218,8 @@ function Card({ name, date, location, price, image, description, organizers, tic
         </div>
       </UseModal>
 
-      <UseModal isOpen={openPayment} onClose={() => setOpenPayment(false)} className="w-[480px] h-[634]">
-        <div>
+      <UseModal isOpen={openPayment} onClose={() => setOpenPayment(false)} className="w-[480px] h-[634] relative">
+        <div className="absolute top-20 left-90">
           <p className="inline-flex gap-1 cursor-pointer font-montserrat font-[400] text-[14px] leading-[22px]">
             <span className="inline-flex items-center justify-center"><ArrowLeft size={18} /> </span>
             Back to Ticket Selection

5614bd5520c970b77de8e6ebdea6ea43fe415ddc
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-08-15 03:03:56 -0700

    refactor: and create a new route for the dashbaord and the profile layout to navigate in the outlet

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Profile from './Pages/Profile';
 import { AuthProvider } from './auth/AuthContext';
+import DashboardLayout from './layout/dashboard-layout';
+import Dashboard from './Pages/dashboard';
+import Tickets from './Pages/tickets';
 
 function App() {
 
@@ -11,8 +14,16 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+
+
+          <Route path='/' element={<DashboardLayout />}>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/tickets' element={<Tickets />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Route>
         </Routes>
+
       </BrowserRouter>
     </AuthProvider>
   );

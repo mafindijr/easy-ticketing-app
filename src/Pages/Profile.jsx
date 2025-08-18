43f8a10bf18c686cec54595af63c12d1f367af91
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-08-18 00:49:32 -0700

    working on the profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import { Input } from "../components/input";
 import { Button } from "../components/button";
 import { Plus } from "lucide-react";
-import { MapPin } from "lucide-react";
 
 export default function Profile() {
 
@@ -10,18 +9,17 @@ export default function Profile() {
 
 
   return (
-    <div className="mt-14">
+    <div className=" my-20 mx-30">
 
             <div className="w-[600px]"> 
-                <h1 className="font-montserrat font-[700] text-[56px] leading-[68px] mb-2">Personal Information</h1>
-                <p className="font-montserrat text-[18px] font-[400] mb-2 leading-[28px]">Update your details to stay connected and book events seamlessly</p>
+                <h2 className="font-montserrat font-[700] text-[56px] leading-[68px] mb-2">My Profile</h2>
             </div>
-        <main className="grid grid-cols-2">
+       <form onSubmit={handleSubmit}>
+        <main className="grid grid-cols-2 gap-8">
+
 
           <div>
-            
-            <form>
-
+            <div className="border-[#cccccc] border-1 rounded-[8px] p-[32px]">
                <div className="grid grid-cols-2 gap-6">
                 <div>
                   <Input
@@ -31,6 +29,7 @@ export default function Profile() {
                       placeholder="First name"
                       register={register}
                       error={errors.firstName}
+                      className={`form-input ${errors.firstName ? "border-red-500 focus:ring-red-500" : ""}`}
                       required
                   />
                 </div>
@@ -42,6 +41,7 @@ export default function Profile() {
                       placeholder="Last name"
                       register={register}
                       error={errors.lastName}
+                      className={`form-input ${errors.lastName ? "border-red-500 focus:ring-red-500" : ""}`}
                       required
                   />
                   </div>
@@ -55,46 +55,34 @@ export default function Profile() {
                     placeholder="Enter your email"
                     register={register}
                     error={errors.email}
+                    className={`form-input ${errors.email ? "border-red-500 focus:ring-red-500" : ""}`}
                     required
                 />
                 </div>
                   <div>
                     <Input
                         label="Phone Number"
-                        name="phone number"
+                        name="phoneNumber"
                         type="text"
                         placeholder="Enter your phone number"
                         register={register}
-                        error={errors.email}
+                        error={errors.phoneNumber}
+                        className={`form-input ${errors.phoneNumber ? "border-red-500 focus:ring-red-500" : ""}`}
                         required
                     />
+
+                     {/* {errors.phoneNumber && (
+                      <span>Please enter Phone Number </span>)}  */}
                  </div>
-                 <div> 
-                  <Input
-                        label="Location"
-                        name="location"
-                        type="text"
-                        placeholder="Enter your location"
-                        register={register}
-                        error={errors.location}
-                        required
-                        prefixIcon={MapPin}
-                    />
-                  </div>
-                  <span className="text-[#4B5563] font-montserrat font-[400] text-[12px] leading-[16px]">Help us recommend neary events</span>
               </div>
-              <div>
-              <button className="w-[300px] h-[60px] mt-4 font-montserrat leading-[28px] font-[400] inline-flex justify-center items-center gap-1 text-[20px] cursor-pointer">
-                <span><Plus size={30}/></span> Add payment method
-              </button>
-              </div>
-              <Button type="submit" onClick={handleSubmit} className="w-[183px] h-[50px] mt-6 font-poppins font-bold bg-blue-600 hover:bg-homeexplohover text-white py-2 rounded-[8px]">
-                    Save Changes
-                </Button>
-            </form>
             </div>
+              <Button type="submit" className="w-[183px] h-[50px] mt-6 font-poppins font-bold bg-blue-600 hover:bg-homeexplohover text-white py-2 rounded-[8px]">
+                    Save Changes
+              </Button>
+            </div>
+
             <div>
-              <form>
+              <div className="border-[#cccccc] border-1 rounded-[8px] p-[32px]">
                 <Input 
                  label="Current Password*"
                  name="password"
@@ -102,6 +90,7 @@ export default function Profile() {
                  placeholder="Current Password"
                  register={register}
                  error={errors.password}
+                 className={`form-input ${errors.phoneNumber ? "border-red-500 focus:ring-red-500" : ""}`}
                  required
                 //  prefixIcon={}
                 />
@@ -113,6 +102,7 @@ export default function Profile() {
                  placeholder="New Password"
                  register={register}
                  error={errors.newPassword}
+                 className={`form-input ${errors.phoneNumber ? "border-red-500 focus:ring-red-500" : ""}`}
                  required
                 //  prefixIcon={}
                 />
@@ -124,12 +114,14 @@ export default function Profile() {
                  placeholder="Confirm Password"
                  register={register}
                  error={errors.confirmPassword}
+                 className={`form-input ${errors.phoneNumber ? "border-red-500 focus:ring-red-500" : ""}`}
                  required
                 //  prefixIcon={}
                 />
-              </form>
+              </div>
             </div>
         </main>
+       </form>
     </div>
   )
 }

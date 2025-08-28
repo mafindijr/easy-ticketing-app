0ed6c29f92fcd67d03ca2083d44fc9b9fa5072c2
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-08-28 01:18:29 -0700

    finnishing styling and import icons from lucide ract for better user experience

--- src/components/events-cards.jsx
+++ src/components/events-cards.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { 
+    CalendarDays,
+    Clock,
+    MapPin,
+    Ticket,
+ } from 'lucide-react'
 
 export default function OrganizersDashboardEventsCards() {
   return (
@@ -10,38 +16,38 @@ export default function OrganizersDashboardEventsCards() {
               </div>
               <div className="flex flex-col gap-2 text-[20px] font-montserrat font-[500] leading-full">
                 <div><h4 className="text-[20px] leading-[30px] font-bold font-montserrat mb-2">Tech for Beginners - Online Bootcamp</h4></div>
-                <div>
-                  <p>
-                    <span>icon</span>
+                <div className='inline-flex gap-2'>
+                  <p className='inline-flex gap-1 items-center'>
+                    <span><CalendarDays className='w-[18px] h-[20px]' /></span>
                     <span>Sept 5, 2025</span>
                   </p>
-                  <p>
-                    <span>icon</span>
+                  <p className='inline-flex gap-1 items-center'>
+                    <span><Clock className='w-[18px] h-[20px]' /></span>
                     <span>10:00 AM - 12:30 PM</span>
                   </p>
                 </div>
                 <div>
-                  <p>
-                    <span>icon</span>
+                  <p className='inline-flex gap-1 items-center'>
+                    <span><MapPin className='w-[18px] h-[20px]' /></span>
                     <span>Oniline (Zoom)</span>
                   </p>
                 </div>
-                <div>
-                  <p>
-                    <span>icon</span>
+                <div className='inline-flex gap-2'>
+                  <p className='inline-flex gap-1 items-center'>
+                    <span><Ticket className='w-[18px] h-[20px]' /></span>
                     <span>Paid Ticket</span>
                   </p>
-                  <p>
+                  <p className='inline-flex gap-1'>
                     <span>icon</span>
                     <span>#5000</span>
                   </p>
                 </div>
-                <div>
-                  <span>Edit</span>
-                  <span>View</span>
+                <div className='inline-flex gap-8 mt-4 text-[16px] fonts-montserrat font-[600] leading-[100%] text-homeexplore'>
+                  <span className='cursor-pointer'>Edit</span>
+                  <span className='cursor-pointer'>View</span>
                   <span>
-                    <span>Downloard Attendees CSV</span>
-                    <span>Downloard icon</span>
+                    <span className='cursor-pointer'>Downloard Attendees CSV</span>
+                    <span className='cursor-pointer'>Downloard icon</span>
                   </span>
                 </div>
               </div>

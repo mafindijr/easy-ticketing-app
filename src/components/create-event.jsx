5ceb13f7c07702c910f21e4e549590795888b722
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-01 08:46:34 -0700

    feat: create the for structure

--- src/components/create-event.jsx
+++ src/components/create-event.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 export default function createEventForm() {
   return (
     <>
-        <div></div>
+        <div>
+         <form>
+            <div>
+                <div>
+                    <h4>Create Event</h4>
+                </div>
+                <div>
+                    <span>Basic Info</span>
+                    <div></div>
+                </div>
+                <div>
+                    <span>Date and Location</span>
+                    <div></div>
+                </div>
+                <div>
+                    <span>Set Ticketing Price</span>
+                    <div></div>
+                </div>
+            </div>
+            <Button></Button>
+         </form>
+        </div>
     </>
   )
 }

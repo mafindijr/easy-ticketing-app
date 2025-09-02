d2798571c28ffd54b92a7f560ceae355a45b28c1
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-01 23:59:59 -0700

    working event from cover image

--- src/components/create-event.jsx
+++ src/components/create-event.jsx
@@ -21,14 +21,32 @@ export default function createEventForm() {
                 <span>icon</span>
                 <span>Back to Events</span>
             </div>
-         <form onSubmit={handleSubmit}>
+         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <div>
                     <h4>Create Event</h4>
                 </div>
                 <div>
                     <span>Basic Info</span>
-                    <div></div>
+                    <div>
+                        <div>
+                            <div>
+                                <div><h4>upload Cover Image</h4></div>
+                                <div>
+                                    <Button>
+                                        <span>icon</span>
+                                        <span>Upload image</span>
+                                    </Button>
+                                    <Button>Remove</Button>
+                                </div>
+                                <div>
+                                    <p>
+                                        Click to browse, or drag and drop a file here, png*,jpeg* files under 10MB, 730 x 300px
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <span>Date and Location</span>

ff1268e0a96a518230bbb06e3f63e93b34397590
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-09-05 17:01:34 +0100

    feat: Add CORS and JSON parsing middleware in server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
+app.use(cors());
+app.use(express.json());
 // connectDB();
 
 const PORT = process.env.PORT || 5001;
